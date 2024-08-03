6c70f5895e3076e482b28522ab8f801c2d35cbdf
Sayan1905 <19sayanghosh05@gmail.com>
2024-08-03 18:28:38 +0530

    refactored reset logic (#1)
    Co-authored-by: Sayan Ghosh <sayanvivo19@gmail.com>

---
+++ reset_pass.py
@@ -0,0 +1,6 @@
+username = input("Enter username = ")
+password = input("Enter pass = ")
+if username == "Admin" and password == "Admin":
+    print("You can reset")
+else:
+    print("You cannot reset")
